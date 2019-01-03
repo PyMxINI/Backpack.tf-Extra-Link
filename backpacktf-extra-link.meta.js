b586e3cc632f3c1a025a4d2e6ac786a0e50d5e1c
ZeusJunior <32263615+ZeusJunior@users.noreply.github.com>
2019-01-03 12:42:25 +0100

    Create backpacktf-extra-link.meta.js

---
+++ backpacktf-extra-link.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         TF2 Extra Trading Site Link
+// @namespace    https://steamcommunity.com/profiles/76561198144346135
+// @version      1.0
+// @description  Add a link on bptf to the stn page of that item
+// @author       Zeus_Junior
+// @match        https://backpack.tf/stats/*
+// @grant        none
+// ==/UserScript==
