260fdcda8eca23d7da417d638b05574192068270
ZeusJunior <32263615+ZeusJunior@users.noreply.github.com>
2019-01-03 12:55:05 +0100

    Update backpacktf-extra-link.user.js

--- backpacktf-extra-link.user.js
+++ backpacktf-extra-link.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         TF2 Extra Trading Site Link
+// @name         Backpack.tf Extra Link
 // @namespace    https://steamcommunity.com/profiles/76561198144346135
 // @version      1.0
 // @description  Add a link on bptf to the stn page of that item
