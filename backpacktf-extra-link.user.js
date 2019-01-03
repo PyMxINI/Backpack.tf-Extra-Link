964ed63ecb61705332c9aef095115ebbc7a33b36
ZeusJunior <32263615+ZeusJunior@users.noreply.github.com>
2019-01-03 14:35:23 +0100

    Update
    Haunted items now dont have a button anymore
    Strange unus also dont for the same reason
    Unusuals now have a button too, which links to any effect search for that hat

--- backpacktf-extra-link.user.js
+++ backpacktf-extra-link.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Backpack.tf Extra Link
+// @name         Backpack.tf Extra Link 
 // @namespace    https://steamcommunity.com/profiles/76561198144346135
 // @version      1.1
 // @description  Add a link on bptf to the stn page of that item
