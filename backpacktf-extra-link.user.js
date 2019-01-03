de8c9cb0523b5f441f2e5758592d9a4a91b516fb
ZeusJunior <32263615+ZeusJunior@users.noreply.github.com>
2019-01-03 12:47:36 +0100

    Update backpacktf-extra-link.user.js

--- backpacktf-extra-link.user.js
+++ backpacktf-extra-link.user.js
@@ -6,7 +6,10 @@
 // @author       Zeus_Junior
 // @match        https://backpack.tf/stats/*
 // @grant        none
+// @updateURL    https://github.com/ZeusJunior/Backpack.tf-Extra-Link/raw/master/backpacktf-extra-link.meta.js
+// @downloadURL  https://github.com/ZeusJunior/Backpack.tf-Extra-Link/raw/master/backpacktf-extra-link.user.js
 // ==/UserScript==
+
 
 (function() {
     'use strict';
