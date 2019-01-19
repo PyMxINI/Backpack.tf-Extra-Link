edd64f8bced84f4c29e6bf42920516c9528f4894
ZeusJunior <32263615+ZeusJunior@users.noreply.github.com>
2019-01-19 10:43:54 +0100

    Update backpacktf-extra-link.user.js

--- backpacktf-extra-link.user.js
+++ backpacktf-extra-link.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Backpack.tf Extra Link 
+// @name         Backpack.tf Extra Link local test
 // @namespace    https://steamcommunity.com/profiles/76561198144346135
-// @version      1.1
+// @version      1.1.1
 // @description  Add a link on bptf to the stn page of that item
 // @author       Zeus_Junior
 // @match        https://backpack.tf/stats/*
@@ -18,10 +18,14 @@
     var pathArray = window.location.pathname.split('/');
     var qualityLocation = pathArray[2] // Quality
     var nameLocation = pathArray[3]; // item name
-    if (qualityLocation === 'Haunted'){
+    if (qualityLocation === 'Haunted') {
         // nothing
-    } else if (qualityLocation === 'Strange%20Unusual'){
+    } else if (qualityLocation === 'Strange%20Unusual') {
         // nothing
+    } else if (nameLocation.includes('Festivized%20')) {
+        itemLinkSTN = pathArray[2] + ' ' + nameLocation.substr(13);
+        // $( ".price-boxes" ).append('<a class="price-box" href="item.com" data-tip="top" target="_blank"  title="Scrap.tf"> <img src="https://scrap.tf/favicon.ico"> <div class="text"> <div class="value"> $x </div> <div class="subtitle"> x </div> </div> </a>');
+        $( ".price-boxes" ).append('<a class="price-box" href="https://stntrading.eu/item/tf2/' + itemLinkSTN + '" data-tip="top" target="_blank"  title="Stntrading.eu"> <img src="https://stntrading.eu/assets/img/favicons/favicon.ico"> <div class="text"> <div class="value"> Click me </div> <div class="subtitle"> Pls click </div> </div> </a>');
     } else if (qualityLocation === 'Unusual'){
         itemLinkSTN = pathArray[2] + ' ' + nameLocation + '&isUnusual=1';
         // $( ".price-boxes" ).append('<a class="price-box" href="item.com" data-tip="top" target="_blank"  title="Scrap.tf"> <img src="https://scrap.tf/favicon.ico"> <div class="text"> <div class="value"> $x </div> <div class="subtitle"> x </div> </div> </a>');
