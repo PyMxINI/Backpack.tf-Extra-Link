7e312cd242c36e8785ec467c1b58b44dbd2348db
ZeusJunior <32263615+ZeusJunior@users.noreply.github.com>
2019-07-21 14:33:19 +0200

    Update backpacktf-extra-link.user.js

--- backpacktf-extra-link.user.js
+++ backpacktf-extra-link.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Backpack.tf Extra Link
 // @namespace    https://steamcommunity.com/profiles/76561198144346135
-// @version      1.1.1
+// @version      1.1.11
 // @description  Add a link on bptf to the stn page of that item
 // @author       Zeus_Junior
 // @match        https://backpack.tf/stats/*
 // @grant        none
-// @updateURL    https://github.com/ZeusJunior/Backpack.tf-Extra-Link/raw/master/backpacktf-extra-link.meta.js
+// @updateURL    https://github.com/ZeusJunior/Backpack.tf-Extra-Link/raw/master/backpacktf-extra-link.user.js
 // @downloadURL  https://github.com/ZeusJunior/Backpack.tf-Extra-Link/raw/master/backpacktf-extra-link.user.js
 // ==/UserScript==
 
